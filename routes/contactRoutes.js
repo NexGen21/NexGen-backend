1394b0b374ff00d32199e4a5feb538e968e15f1f
sarthakp10 <sarthakphadnis10@gmail.com>
2025-01-03 03:08:42 +0530

    NexGen1.0 backend

---
+++ routes/contactRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const Contact = require('../models/ContactModel')
+
+router.post('/', async (req, res) => {
+    const { name, email, subject, msg } = req.body
+    console.log(req.body);
+    let emptyFields = []
+    if(!name) {
+        emptyFields.push('name')
+    }
+    if(!email) {
+        emptyFields.push('email')
+    }
+    if(emptyFields.length > 0) {
+        return res.status(400).json({error: "Please provide your name and email", emptyFields})
+    }
+    try {
+        const cont = await Contact.create({ name, email, subject, msg })
+        return res.status(200).json(cont);
+    }
+    catch(error) {
+        return res.status(400).json({error: error.message})
+    }
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const contactRoutes = require('./routes/contactRoutes')
+const cors = require('cors')
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:4000"
+}))
+
+// Middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+app.use('/api', contactRoutes);
+
+// Connecting to database
+mongoose.connect('mongodb+srv://sarthak:sarthak@nexgen.doemdgx.mongodb.net/?retryWrites=true&w=majority&appName=NexGen')
+    .then(() => {
+        app.listen(4000, () => {
+            console.log("Connected to database. Listening to port 4000");
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
